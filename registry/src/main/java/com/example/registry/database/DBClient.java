c4955ea085063d90e153e75036c70ecef9d609c0
Yashpatel20 <yash.work10@gmail.com>
2020-07-12 09:21:23 +0530

    service registry spring boot setup

---
+++ registry/src/main/java/com/example/registry/database/DBClient.java
@@ -0,0 +1,5 @@
+package com.example.registry.database;
+
+public class DBClient {
+    
+}
---
+++ registry/src/main/java/com/example/registry/service/ServiceRegistry.java
@@ -0,0 +1,10 @@
+package com.example.registry.service;
+
+import com.example.registry.database.DbClient;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceRegistry {
+
+}
